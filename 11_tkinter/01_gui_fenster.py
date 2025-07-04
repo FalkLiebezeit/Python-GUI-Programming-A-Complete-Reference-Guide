c3aa5b8609e6f084be1239c50ef8655290d6314a
@FalkLiebezeit <falk.liebezeit@outlook.com>
2025-07-04 22:42:14 +0200

    040725

--- 11_tkinter/01_gui_fenster.py
+++
@@ -1,6 +0,0 @@
-#01_gui_fenster.py
-import tkinter as tk                                                                     
-fenster=tk.Tk()                                                                          
-lblErgebnis=tk.Label(fenster,text="Ergebnisausgabe",font=("Arial 24"))    
-lblErgebnis.pack()
-fenster.mainloop()                                                                         
